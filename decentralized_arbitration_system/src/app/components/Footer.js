b5d8162e422869991cb6e5ccf3d9fa3586750e60
SanishPagui <sanishpagui162@gmail.com>
2024-10-19 23:36:22 +0530

    footer

--- decentralized_arbitration_system/src/app/components/Footer.js
+++ decentralized_arbitration_system/src/app/components/Footer.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full h-full bg-[#F2E3FF]'>
-        <div className='bg-[#4D00B4] text-white'>
-        <div className='pl-12 text-xl py-3'>
-          Find out more about Frosche
+    <div className='w-full h-full bg-[#F2E3FF] '>
+        <div className='bg-[#4D00B4] py-3 text-md text-white flex items-center'>
+          <div className='basis-1/3 pl-20'>
+            Find out more about Frosche
+          </div>
+          <div className=' basis-1/3 justify-center flex'>
+            Frosche
+          </div>
+          <div className='basis-1/3 flex justify-end pr-20'>
+            Need Help?
+          </div>
+          <div></div>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
     </div>
   )
 }
