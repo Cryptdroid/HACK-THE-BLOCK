9350bc8c4bef9ea9f9a1e836a71307a076e1814d
Cleophus Alves <cleophus775@gmail.com>
2024-10-19 22:02:21 +0530

    Update Footer.js

--- decentralized_arbitration_system/src/app/components/Footer.js
+++ decentralized_arbitration_system/src/app/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='w-full h-full bg-[#F2E3FF]'>
         <div className='bg-[#4D00B4] text-white'>
         <div className='pl-12 text-xl py-3'>
-          Find out more about Chirag
+          Find out more about Frosche
         </div>
         <div></div>
         <div></div>
