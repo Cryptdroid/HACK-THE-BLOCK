ae3c85b5ca20386e1d0d8e2e746a754ba6e19f49
SanishPagui <sanishpagui162@gmail.com>
2024-10-20 10:05:50 +0530

    Loading animation

--- decentralized_arbitration_system/src/app/page.js
+++ decentralized_arbitration_system/src/app/page.js
@@ -1,12 +1,26 @@
 'use client'
 
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Dashboard from './components/Dashboard'
 import Footer from './components/Footer'
+import './styles/styles.scss'
 
 const page = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+  if (loading) {
+    return (
+      <div className="loader">Loading...</div>
+    );
+  }
   return (
     <div className=' w-full h-full bg-[#F2E3FF]'>
       <Navbar/>
